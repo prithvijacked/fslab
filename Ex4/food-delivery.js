ba2ebc63742288d443b3ac84d13b045d1fc7ad56
prithvijacked <157978525+prithvijacked@users.noreply.github.com>
2024-11-19 11:41:10 +0530

    Update food-delivery.js

--- Ex4/food-delivery.js
+++ Ex4/food-delivery.js
@@ -10,7 +10,7 @@ const restaurantData = {
     2: {
         name: "M'cDonald's",
         menu: [
-            { name: "Big Mac", price: 3.99, image: "bigmac.jpg" },
+            { name: "Big Mac", price: 3.99, image: "burger.jpg" },
             { name: "Fries", price: 1.99, image: "fries.jpg" },
             { name: "McFlurry", price: 2.49, image: "mcflurry.jpg" }
         ]
@@ -19,14 +19,14 @@ const restaurantData = {
         name: "Burger King",
         menu: [
             { name: "Whopper", price: 4.99, image: "whopper.jpg" },
-            { name: "Onion Rings", price: 2.29, image: "onionrings.jpg" },
-            { name: "Ice Cream", price: 1.89, image: "icecream.jpg" }
+            { name: "Onion Rings", price: 2.29, image: "onion.jpg" },
+            { name: "Ice Cream", price: 1.89, image: "ice.jpg" }
         ]
     },
     4: {
         name: "Los Pollos Hermanos",
         menu: [
-            { name: "Chicken Platter", price: 7.99, image: "chicken.jpg" },
+            { name: "Chicken Platter", price: 7.99, image: "chicplat.jpg" },
             { name: "Tortilla", price: 0.99, image: "tortilla.jpg" },
             { name: "Salsa", price: 1.29, image: "salsa.jpg" }
         ]
